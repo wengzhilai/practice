d529db3efe454737413fc3cb92749936583061ce
System Administrator <root@isoftdeMac-mini.local>
2018-05-08 10:04:34 +0800

    添加五子棋

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import os, subprocess, sys
+subprocess.call(['pip', 'install', 'flask'])
+subprocess.call(['pip', 'install', 'flask-login'])
+subprocess.call(['pip', 'install', 'flask-openid'])
+subprocess.call(['pip', 'install', 'flask-mail'])
+subprocess.call(['pip', 'install', 'sqlalchemy'])
+subprocess.call(['pip', 'install', 'flask-sqlalchemy'])
+subprocess.call(['pip', 'install', 'sqlalchemy-migrate'])
+subprocess.call(['pip', 'install', 'flask-whooshalchemy'])
+subprocess.call(['pip', 'install', 'flask-wtf'])
+subprocess.call(['pip', 'install', 'flask-babel'])
+subprocess.call(['pip', 'install', 'flask-cors'])
+subprocess.call(['pip', 'install', 'Flask-HTTPAuth'])
+subprocess.call(['pip', 'install', 'flask-sqlacodegen'])
+subprocess.call(['pip', 'install', 'pyMySQL'])
+subprocess.call(['pip', 'install', 'numpy'])
+subprocess.call(['pip', 'install', 'openpyxl'])
